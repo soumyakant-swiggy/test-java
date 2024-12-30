3ef01117130998e08ac99a42a4b61f6ffcff046c
soumyakant-swiggy <soumyakant.swain_ftc@external.swiggy.in>
2024-12-30 15:58:16 +0530

    test-commit

--- src/main/java/com/freightfox/pdf/DynamicPdfGeneratorApplication.java
+++ src/main/java/com/freightfox/pdf/DynamicPdfGeneratorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DynamicPdfGeneratorApplication {
 
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(DynamicPdfGeneratorApplication.class, args);
 	}
